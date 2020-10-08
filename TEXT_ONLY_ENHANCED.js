f3f5dd249e319ac9d1743cd031c1347e442706ad
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-08 20:34:46 +0300

    Big leap. Array.From is amazing

--- TEXT_ONLY_ENHANCED.js
+++ TEXT_ONLY_ENHANCED.js
@@ -1,79 +1,58 @@
+// 1. hasSiblingTop, hasSiblingBot, hasParent,hasChild #text
+// 2. Add on top of the replaced content an absolute element containing div > textNode, colors etc
+
 // Helpers
-var style = `
-    background: #666666 !important; 
-    color: #666666 !important; 
-    border: 1px solid #A6A6A6 !important; 
-    text-shadow: none !important;
-`;
+    var styled = `
+        background: #666666 !important; 
+        color: #666666 !important; 
+        border: 1px solid #A6A6A6 !important; 
+        text-shadow: none !important;
+    `;
+    function ifFirstChildExistsAndIsTextNodeAndNotEmpty(el){//I will probably need to check all children in a next iteration
+        if(el.firstChild && el.firstChild.textContent.trim().length > 0 && el.firstChild.nodeType === 3){
+            wrapTextNodeInAMark(el)
+        }
+    }
+    function wrapTextNodeInAMark(el){
+        let textNodeTextContent = el.firstChild.textContent.trim();
+        let textNodeParent = el.firstChild.parentElement;
 
-async function wrapAnyNodeTypeThreeChildrenWithinMark(node){
-    let innerNodes = node.childNodes;
-    innerNodes.forEach(child=>{
+        let mark = document.createElement("mark"); mark.style = styled;mark.innerText = textNodeTextContent;
     
-        if(child.nodeType == 3){
-            // p.style.background = "#b2b2b2"
-            nodeTypeisThreeThenReplaceWithMark(child)
+        textNodeParent.textContent = "";
+        textNodeParent.appendChild(mark);
+    }
+    function replaceColorAndOthers(el){
+        elem.style = styled;
+    }
+
+
+//
+
+// MAIN. With only one QUERY of all elements :)
+    Array.from(document.body.querySelectorAll('*')).forEach(el => {
+        if(el.nodeName == "SCRIPT"){
+            //do nothing
+        }
+        else if(el.nodeName == "DIV"){
+            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+        } 
+        else if(el.nodeName == "A"){
+            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+        }
+        else if(el.nodeName == "SPAN"){
+            el.style= styled;
+            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+        }
+        else if(el.nodeName == "TD"){
+            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+        }
+        else if(el.nodeName == "H1" || el.nodeName == "H2" || el.nodeName == "H3" || el.nodeName == "H4" || el.nodeName == "H5"){
+            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+        }
+        else if(el.nodeName == "INPUT"){
+            el.style= styled;
         }
     })
-}
-async function nodeTypeisThreeThenReplaceWithMark(node){
-    if(node){
-        if(node.nodeName == "#text" && node.textContent !== ""){
-            let text = node.textContent.trim();
-            let mark = document.createElement("mark")
-            mark.innerText = text;
-            mark.style = `
-                background: #666666 !important; 
-                color: #666666 !important; 
-                border: 1px solid #A6A6A6 !important; 
-                text-shadow: none !important;
-            `
-            node.textContent = "";
-            node.parentElement.appendChild(mark);
-        }
-    }
-}
-async function isFirstNodeTextThenReplaceWithMark(node){
-    if(node.firstChild){
-        if(node.firstChild.nodeName == "#text"){
-            let text = node.firstChild.textContent.trim();
-            let mark = document.createElement("mark")
-            mark.innerText = text;
-            node.textContent = "";
-            mark.style = style;
-            node.appendChild(mark);
-        }
-    }
-}
-function replaceText(node){
-    document.querySelectorAll(node).forEach(node=>{  wrapAnyNodeTypeThreeChildrenWithinMark(node) })
-}
-function replaceFirstText(node){
-    document.querySelectorAll(node).forEach(node=>{  isFirstNodeTextThenReplaceWithMark(node) })
-}
-function replaceColor(node){
-    var nodes = document.querySelectorAll(node);
-    nodes.forEach(elem=>{  
-        elem.style = style
+//
 
-    })
-
-}
-
-
-async function main(){
-    replaceText("a")
-
-    replaceText("td")
-
-    replaceText("h1");
-    replaceText("h2");
-    replaceText("h3");
-    replaceText("h4");
-    replaceText("h5");
-
-    replaceText("span")
-    replaceColor("span")
-
-}
-main();
---
+++ archive/OLD_TEXT_GOOD.js
@@ -0,0 +1,83 @@
+// Helpers
+var styled = `
+    background: #666666 !important; 
+    color: #666666 !important; 
+    border: 1px solid #A6A6A6 !important; 
+    text-shadow: none !important;
+`;
+
+async function wrapAnyNodeTypeThreeChildrenWithinMark(node){
+    let innerNodes = node.childNodes;
+    innerNodes.forEach(child=>{
+    
+        if(child.nodeType == 3){
+            // p.style.background = "#b2b2b2"
+            nodeTypeisThreeThenReplaceWithMark(child)
+        }
+    })
+}
+async function nodeTypeisThreeThenReplaceWithMark(node){
+    if(node){
+        if(node.nodeName == "#text" && node.textContent !== ""){
+            let text = node.textContent.trim();
+            let mark = document.createElement("mark")
+            mark.innerText = text;
+            mark.style = `
+                background: #666666 !important; 
+                color: #666666 !important; 
+                border: 1px solid #A6A6A6 !important; 
+                text-shadow: none !important;
+            `
+            node.textContent = "";
+            node.parentElement.appendChild(mark);
+        }
+    }
+}
+async function isFirstNodeTextThenReplaceWithMark(node){
+    if(node.firstChild){
+        if(node.firstChild.nodeName == "#text"){
+            let text = node.firstChild.textContent.trim();
+            let mark = document.createElement("mark")
+            mark.innerText = text;
+            node.textContent = "";
+            mark.style = styled;
+            node.appendChild(mark);
+        }
+    }
+}
+function replaceText(node){
+    document.querySelectorAll(node).forEach(node=>{  wrapAnyNodeTypeThreeChildrenWithinMark(node) })
+}
+function replaceFirstText(node){
+    document.body.querySelectorAll(node).forEach(node=>{  isFirstNodeTextThenReplaceWithMark(node) })
+}
+function replaceColor(node){
+    var nodes = document.querySelectorAll(node);
+    nodes.forEach(elem=>{  
+        elem.style = styled;
+
+    })
+
+}
+
+
+async function main(){
+    replaceText("a")
+
+    replaceText("td")
+
+    replaceText("h1");
+    replaceText("h2");
+    replaceText("h3");
+    replaceText("h4");
+    replaceText("h5");
+
+    replaceText("span")
+    replaceColor("span")
+
+
+    replaceFirstText("div")
+}
+main();
+
+
