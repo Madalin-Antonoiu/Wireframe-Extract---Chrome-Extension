f9f90c4b22e9a7567a0d003face60a232afdc998
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-08 21:37:37 +0300

    Part 1 Completed - It hides TEXT

--- TEXT_ONLY_ENHANCED.js
+++ TEXT_ONLY_ENHANCED.js
@@ -9,23 +9,46 @@
         text-shadow: none !important;
     `;
     function ifFirstChildExistsAndIsTextNodeAndNotEmpty(el){//I will probably need to check all children in a next iteration
+        
+        // Helper
+        function wrapTextNodeInAMark(el){
+            if(el.firstChild){
+                let textNodeTextContent = el.firstChild.textContent.trim();
+                let textNodeParent = el.firstChild.parentElement;
+        
+                let mark = document.createElement("mark"); mark.style = styled;mark.innerText = textNodeTextContent;
+            
+                textNodeParent.textContent = "";
+                textNodeParent.appendChild(mark);
+            }
+    
+        }
+        
         if(el.firstChild && el.firstChild.textContent.trim().length > 0 && el.firstChild.nodeType === 3){
             wrapTextNodeInAMark(el)
         }
     }
-    function wrapTextNodeInAMark(el){
-        let textNodeTextContent = el.firstChild.textContent.trim();
-        let textNodeParent = el.firstChild.parentElement;
+    function ifAnyChildExistsAndIsTextNodeAndNotEmpty(el){//I will probably need to check all children in a next iteration
 
-        let mark = document.createElement("mark"); mark.style = styled;mark.innerText = textNodeTextContent;
-    
-        textNodeParent.textContent = "";
-        textNodeParent.appendChild(mark);
-    }
-    function replaceColorAndOthers(el){
-        elem.style = styled;
-    }
+        let children = el.childNodes;
+        children.forEach( child =>{
+            if(child.textContent && child.textContent.trim().length > 0 && child.nodeType === 3){
+                wrapTHISTextNodeInAMark(child)
+            }
+        })
 
+
+        // Helper
+        function wrapTHISTextNodeInAMark(el){
+            let textNodeTextContent = el.textContent.trim();
+            let textNodeParent = el.parentElement;
+
+            let mark = document.createElement("mark"); mark.style = styled;mark.innerText = textNodeTextContent;
+        
+            textNodeParent.textContent = "";
+            textNodeParent.appendChild(mark);
+        }
+    }
 
 //
 
@@ -35,20 +58,21 @@
             //do nothing
         }
         else if(el.nodeName == "DIV"){
-            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+            // ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+            ifAnyChildExistsAndIsTextNodeAndNotEmpty(el);
         } 
         else if(el.nodeName == "A"){
-            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+            ifAnyChildExistsAndIsTextNodeAndNotEmpty(el);
         }
         else if(el.nodeName == "SPAN"){
             el.style= styled;
-            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+            ifAnyChildExistsAndIsTextNodeAndNotEmpty(el);
         }
         else if(el.nodeName == "TD"){
-            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+            ifAnyChildExistsAndIsTextNodeAndNotEmpty(el);
         }
         else if(el.nodeName == "H1" || el.nodeName == "H2" || el.nodeName == "H3" || el.nodeName == "H4" || el.nodeName == "H5"){
-            ifFirstChildExistsAndIsTextNodeAndNotEmpty(el)
+            ifAnyChildExistsAndIsTextNodeAndNotEmpty(el);
         }
         else if(el.nodeName == "INPUT"){
             el.style= styled;
